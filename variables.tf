f38d681ccf65b1485b444f588ab9d63c15fe6ff6
pawan salve <pawansalwe470@gmail.com>
2026-06-24 22:44:30 +0530

    split the tarraform script

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  type        = string
-  description = "The target deployment environment (dev or qa)"
-}
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
