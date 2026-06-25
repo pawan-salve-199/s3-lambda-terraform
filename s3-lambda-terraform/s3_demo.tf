3cdc8ee88778914fcb267b9561ebaca52f0fbae3
pawan salve <pawansalwe470@gmail.com>
2026-06-25 16:06:35 +0530

    demofile

--- s3-lambda-terraform/s3_demo.tf
+++ s3-lambda-terraform/s3_demo.tf
@@ -1,8 +1,8 @@
-# resource "aws_s3_bucket" "s3_bucket_demo" {
-#   bucket = "my-tf-test-bucket${var.environment}-221"
+resource "aws_s3_bucket" "s3_bucket_demo" {
+  bucket = "my-tf-test-bucket${var.environment}-221"
 
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
