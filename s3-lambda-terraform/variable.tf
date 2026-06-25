37c211ce0dab7a27e98a52423e002b6e60af557d
pawan salve <pawansalwe470@gmail.com>
2026-06-25 14:25:34 +0530

    change the file name

---
+++ s3-lambda-terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "environment" { 
+  type        = string
+  description = "The target deployment environment (dev or qa)"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
