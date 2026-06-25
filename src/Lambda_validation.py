5772c051d9ea96a9328953ee957e59b02e26c778
pawan salve <pawansalwe470@gmail.com>
2026-06-25 13:06:09 +0530

    made changes in validation script

--- src/Lambda_validation.py
+++ src/Lambda_validation.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
         
-        print(f"SUCCESS: New file '{key}' validated")
+        print(f"SUCCESS: New file '{key}' validated!")
         
         return {
             'statusCode': 200,
