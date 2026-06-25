52539932fb0f5ecfd0a804bdcc2628435aa24bca
pawan salve <pawansalwe470@gmail.com>
2026-06-25 14:31:59 +0530

    iam

---
+++ s3-lambda-terraform/iam.tf
@@ -0,0 +1,19 @@
+# 1. IAM Security Role for your Lambda to run safely
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-s3-trigger-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# 2. Attach log-writing permissions so your Lambda can print messages
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
